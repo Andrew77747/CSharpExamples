dd20d0701aa85a9ca360d38902c71f60edb303d4
Андрей <>
2025-06-08 22:23:46 +0300

    добавил проект TryParse

---
+++ TryParse/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TryParse
+{
+    class Program
+    {
+        private static bool TryParse(string s, out bool res)
+        {
+            if (s == "false" || s == "true")
+            {
+                res = s == "true";
+                return true;
+            }
+
+            res = false;
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            if (TryParse("false", out var v))
+            {
+                Console.WriteLine("Success " + v);
+            }
+        }
+    }
+}
